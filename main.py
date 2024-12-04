069e3a64a00ec9b2954f8433559e67623f016ad8
Grant Keller <gwkeller@iu.edu>
2024-12-03 21:00:52 -0500

    Remove mysql

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import render_template, request
 from src import create_app
-from src.utils import connect_to_mysql, query_county
 import requests
 
 app = create_app()
@@ -8,13 +7,6 @@ app = create_app()
 @app.route('/')
 def get_home():
     return render_template("base.html")
-
-@app.route('/county', methods=['GET'])
-def get_county_info():
-    con = connect_to_mysql()
-    county = request.args.get('county')
-    result = query_county(con, county)
-    return result, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
