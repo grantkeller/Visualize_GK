c42cb64541b88c618df2c0d8e74d94f1d52b4b44
Grant Keller <gwkeller@iu.edu>
2024-12-03 21:52:37 -0500

    Working temps

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = create_app()
 def get_home():
     return render_template("base.html")
 
+@app.route('/about')
+def get_about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
