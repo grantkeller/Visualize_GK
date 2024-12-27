19f83955af798f4ad8eb766b4fee57184fe625e9
Grant Keller <gwkeller@iu.edu>
2024-12-26 19:57:18 -0500

    Separate pages change

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ app = create_app()
 
 @app.route('/')
 def get_home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 @app.route('/about')
 def get_about():
     return render_template("about.html")
+
+@app.route('/hometown')
+def get_hometown():
+    return render_template("hometown.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
